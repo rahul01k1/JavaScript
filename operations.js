aa83e568e062a09b63f7595699026431a31dda36
Vipul <lilaparavipul123@gmail.com>
2026-07-22 00:03:32 +0530

    complete operations

---
+++ operations.js
@@ -0,0 +1,50 @@
+// operations  like addition , substraction , division , multiplication , modulo , power  , floor values
+
+
+
+let a = 2;
+
+let b = 3;
+
+let addition = a + b ;
+
+let substraction = a - b ;
+
+let multiplication = a * b ;
+
+let division = a / b;
+
+let modulo = a % b ;
+
+let power = a ** b;
+
+
+// console.log(addition);
+// console.log(substraction);
+// console.log(multiplication);
+// console.log(division);
+// console.log(modulo);
+// console.log(power);
+
+
+// concatanation of str 
+
+
+let userName = "my name is Rahul"
+
+let age = "I'm 23 Years Old.."
+
+let details = userName + age
+
+console.log(details);
+
+
+console.log('1' + 2); // string : 12
+
+console.log(1 + '2'); // string : 12
+
+console.log('1' + 2 + 3); // string 123
+
+console.log(1 + 2 + '3'); // 33
+
+
