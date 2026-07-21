f0fed938f19d553879f83553903c5f71bd7177f0
Vipul <lilaparavipul123@gmail.com>
2026-07-21 23:14:18 +0530

    datatypes complete

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+let name = "Rahul"; // string
+
+let age =20.7990; // number  and anather is big int
+
+let isTeen = true; // bool 
+
+value = null; //null
+
+let  later; // undefined
+
+let person = {
+    //block of code 
+}
+
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isTeen));
+console.log(typeof(value));
+console.log(typeof(later));
+console.log(typeof(person));
+
