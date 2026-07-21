447d4dcec89727a42553cd2feb8e9899d54ce686
Vipul <lilaparavipul123@gmail.com>
2026-07-22 00:16:54 +0530

    comparisons and its logic complete

---
+++ comparisions.js
@@ -0,0 +1,22 @@
+
+console.log( null > 0); //false
+
+console.log(null == 0); //false
+
+console.log(null <= 0); //true
+
+console.log( null >= 0); // true
+
+console.log( null === 0); //false
+
+console.log('2' === 2); // false 
+
+
+console.log('2' > 1); // convertvalue : true 
+
+console.log('02' < 1); // false 
+
+
+
+
+
