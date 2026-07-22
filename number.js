128aed5c5e8a62549d453c67d41b8e1c98659c72
Vipul <lilaparavipul123@gmail.com>
2026-07-23 00:27:52 +0530

    numbers complete

---
+++ number.js
@@ -0,0 +1,67 @@
+// const Age = 24
+
+// console.log(Age); // 24
+
+// const balance = new Number(102820)
+
+// console.log(balance); // [Number : 102820]
+
+
+// let avg = 24
+
+// Avg = avg.toString().length
+
+// console.log(typeof Avg);
+// console.log(Avg);
+
+
+// // fixed 
+
+// Marks  = avg.toFixed(2)
+
+// console.log(Marks);
+
+
+// // precison
+
+// let numbers = 23.7872
+
+// // let precise_no = numbers.toPrecision(3)  // pass argument of digit , how many numbers from start you consider 
+
+// // console.log(precise_no);
+
+// // tolocalString
+
+
+
+// let num = 1000000
+
+// let precise_no = num.toLocaleString('en-IN')  // 10,00,000
+
+// console.log(precise_no);
+
+
+// // min max 
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+
+console.log(Math);
+
+console.log(Math.abs(-10));
+
+console.log(Math.sqrt(25));
+
+console.log(Math.round(10.6678));
+
+console.log(Math.random()); // value btwn 0 to 1
+
+console.log(Math.ceil(3.9));
+
+console.log(Math.floor(3.9));
+
+console.log(Math.max(2,8,9,30,36));
+console.log(Math.min(2,8,9,30,36));
+
+
