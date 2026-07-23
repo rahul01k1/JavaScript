f6d7a3d304224bfa435a12bfa93a5e69e7a5bb60
Vipul <lilaparavipul123@gmail.com>
2026-07-24 00:32:47 +0530

    maths , date and time complete

---
+++ dateAndtime.js
@@ -0,0 +1,32 @@
+
+let mydate = new Date();
+
+// console.log(mydate.toDateString());
+
+// console.log(mydate.toISOString());
+
+// console.log(mydate.toString());
+
+// console.log(mydate.toJSON());
+
+// console.log(mydate.toLocaleDateString());
+
+// console.log(mydate.toLocaleString());
+
+
+let created_date = new Date('2026-01-21')
+
+console.log(created_date.toLocaleString());
+
+
+let timestamp = Date.now()
+
+console.log(timestamp);
+
+let new_time = created_date.getTime()
+console.log(new_time);
+
+// let res =  new_time  - timestamp
+
+// console.log(res);
+
